2e381032ad9a1a73aeb928edaa28dcbfd289e1a0
arcsurge <49866223+arcsurge@users.noreply.github.com>
2024-08-06 05:14:11 +0800

    :tada: 初始化

---
+++ src/main/java/top/eliauk/arkscycle/ArksCycleApplication.java
@@ -0,0 +1,13 @@
+package top.eliauk.arkscycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArksCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArksCycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/eliauk/arkscycle/ArksCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package top.eliauk.arkscycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArksCycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
